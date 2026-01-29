b5a641ebcfe7e53574a51c3b74cac15babc3014e
GeTimS <getims@yandex.ru>
2026-01-29 19:21:04 +0300

    Data bug fix

--- ArmyClashTest/Assets/Project/Scripts/Data/TeamData.cs
+++ ArmyClashTest/Assets/Project/Scripts/Data/TeamData.cs
@@ -6,7 +6,7 @@ namespace Project.Scripts.Data
     [Serializable]
     public class TeamData
     {
-        public UnitTeam UnitTeam { get; private set; }
+        public UnitTeam UnitTeam;
         public int WinsCount = 0;
 
         public TeamData(UnitTeam unitTeam)
