4dcad63696ba0549922306d02738a4454b255f6a
GeTimS <getims@yandex.ru>
2026-01-29 16:33:18 +0300

    Remove unused module

--- ArmyClashTest/Assets/Project/Scripts/Configs/GlobalConfig.cs
+++ ArmyClashTest/Assets/Project/Scripts/Configs/GlobalConfig.cs
@@ -8,9 +8,6 @@ namespace Project.Scripts.Configs
     [Serializable]
     public class GlobalConfig : ScriptableConfig
     {
-        [SerializeField]
-        private bool _enableDebug;
-
         [SerializeField, MinValue(1)]
         private int _unitsCountPerTeam = 20;
 
@@ -21,7 +18,6 @@ namespace Project.Scripts.Configs
         [SerializeField]
         private float _attackSpeedPointValue = 1;
 
-        public bool EnableDebug => _enableDebug;
         public float SpeedPointValue => _speedPointValue;
         public float AttackSpeedPointValue => _attackSpeedPointValue;
         public int UnitsCountPerTeam => _unitsCountPerTeam;
